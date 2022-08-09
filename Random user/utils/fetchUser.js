3402c60a7b5a83d9218717e155ff8b0022fa6c0b
Vera <58885611+barcelo2@users.noreply.github.com>
2022-08-09 14:07:01 +0200

    Add files via upload

---
+++ Random user/utils/fetchUser.js
@@ -0,0 +1,31 @@
+
+
+const url='https://randomuser.me/api/'
+
+const getUser=async()=>{
+    const response= await fetch(url)
+    const data= await response.json()
+   
+const person =data.results[0]
+console.log(person);
+
+const {phone,email}=person
+const {large:image}=person.picture
+const {password}=person.login
+const {first,last}=person.name
+const{age}=person.dob
+const{street:{number, name}}=person.location
+
+
+return{
+    phone,
+    email,
+    image,
+    password,
+    name:`${first} ${last}`,
+    age,
+    street:`${number} ${name}`
+}
+}
+
+export default getUser;
